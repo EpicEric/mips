d2e678a68a344bd476e9a562d0cc49b8abe65c3b
Eric Pires <ericpires9@gmail.com>
2016-12-13 21:52:14 +0000

    Updated verification for test2.asm

--- mipssingle.vhd
+++ mipssingle.vhd
@@ -40,7 +40,7 @@ begin
   -- check that 7 gets written to address 84 at end of program
   process (clk) begin
     if (clk'event and clk = '0' and memwrite = '1') then
-      if (to_integer(dataadr) = 84 and to_integer(writedata) = 7) then 
+      if (to_integer(dataadr) = 84 and to_integer(writedata) = -33022) then 
         report "NO ERRORS: Simulation succeeded" severity failure;
       elsif (dataadr /= 80) then 
         report "Simulation failed" severity failure;
